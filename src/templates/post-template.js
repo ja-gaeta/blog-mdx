d0bb740e81694cf5b47708b4b2aee53bcd05f40d
Jose Mendes <ja.gaeta@hotmail.com>
2020-02-06 21:54:37 -0300

    Added PostList, PostCard and postTemplate

---
+++ src/templates/post-template.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "../css/postTemplate.module.css"
+import { Link, graphql } from "gatsby"
+import Image from "gatsby-image"
+import Layout from "../components/Layout"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+
+const postTemplate = () => {
+  return <div>this is a post template</div>
+}
+
+export default postTemplate
